e15800b9e85ef8d8804d0411d266078921b01157
ThomasCl <56159161+ThomasCl@users.noreply.github.com>
2021-02-19 15:41:56 +0100

    week2gemaakt

--- week02_bomen/src/domain/BinaryTree.java
+++ week02_bomen/src/domain/BinaryTree.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class BinaryTree<E> {
 	private E data;
 	private BinaryTree<E> leftTree, rightTree;
@@ -19,8 +21,89 @@ public class BinaryTree<E> {
 	
 	public void printPreorder(){
 			System.out.print(this.data + " ");
-			if (this.leftTree != null) this.leftTree.printPreorder();
-			if (this.rightTree != null) this.rightTree.printPreorder();
+		if (this.leftTree != null) this.leftTree.printPreorder();
+		if (this.rightTree != null) this.rightTree.printPreorder();
+	}
+	public void printInorder(){
+		if (this.leftTree != null) this.leftTree.printInorder();
+		System.out.print(this.data + " ");
+		if (this.rightTree != null) this.rightTree.printInorder();
+	}
+	public void printPostorder(){
+		if (this.leftTree != null) this.leftTree.printPostorder();
+		if (this.rightTree != null) this.rightTree.printPostorder();
+		System.out.print(this.data + " ");
 	}
 
+	public int countNodes(){
+		if(leftTree == null && rightTree == null){
+			return 1;
+		}
+		if(leftTree == null){
+			return(1+ rightTree.countNodes());
+		}
+		if(rightTree == null){
+			return(1+ leftTree.countNodes());
+		}
+
+		return(1 + leftTree.countNodes()+ rightTree.countNodes());
+	}
+
+	public int getDepth(){
+		if(leftTree == null && rightTree == null){
+			return 1;
+		}
+		if(leftTree == null){
+			return(1+ rightTree.getDepth());
+		}
+		if(rightTree == null){
+			return (1 + leftTree.getDepth());
+		}
+
+		int i = leftTree.getDepth();
+		int j = rightTree.getDepth();
+		if(i > j){
+			return i+1;
+		}
+		return j+1;
+	}
+
+	public boolean isLeaf(){
+		if(leftTree == null && rightTree == null){
+			return true;
+		}
+		return false;
+	}
+
+	public int countleaves(){
+		if(isLeaf()){
+			return 1;
+		}
+		if(leftTree == null){
+			return rightTree.countleaves();
+		}
+		if(rightTree == null){
+			return leftTree.countleaves();
+		}
+		return(leftTree.countleaves() + rightTree.countleaves());
+	}
+
+	public ArrayList<E> getDataLeaves(){
+		ArrayList<E> list = new ArrayList<E>();
+		if(isLeaf()){
+			list.add(this.data);
+			return list;
+		}
+		if(leftTree == null){
+			return rightTree.getDataLeaves();
+		}
+		if(rightTree == null){
+			return leftTree.getDataLeaves();
+		}
+		list = rightTree.getDataLeaves();
+		for(E i: leftTree.getDataLeaves()){
+			list.add(i);
+		}
+		return(list);
+	}
 }
